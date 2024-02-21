2bfec3c896418d63ca0788ff64fe9183c48c15e2
Simon Kennedy <sffjunkie+code@gmail.com>
2024-02-21 16:24:15 +0000

    Disable logrotate check

--- configuration/machine/babs/default.nix
+++ configuration/machine/babs/default.nix
@@ -25,6 +25,7 @@
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
     services.nfs.server.enable = true;
+    services.logrotate.checkConfig = false;
 
     looniversity = {
       service = {
