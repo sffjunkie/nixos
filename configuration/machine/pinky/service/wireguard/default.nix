a4c2a11149b8a57839b8efe2bd14cad2b5bdd8ac
Simon Kennedy <sffjunkie+code@gmail.com>
2024-02-23 15:53:51 +0000

    Delete comments

--- configuration/machine/pinky/service/wireguard/default.nix
+++ configuration/machine/pinky/service/wireguard/default.nix
@@ -21,13 +21,10 @@ in {
 
         listenPort = 51820;
 
-        # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
-        # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
         postSetup = ''
           ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.50.0.0/24 -o ${wanDev} -j MASQUERADE
         '';
 
-        # This undoes the above command
         postShutdown = ''
           ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.50.0.0/24 -o ${wanDev} -j MASQUERADE
         '';
