277713be3e69215f247ea9c001fd13f10a9b6bd1
Simon Kennedy <sffjunkie+code@gmail.com>
2024-02-24 09:12:17 +0000

    Add a `nixpkgs-repl` function to start a repl with nixpkgs loaded
    If no nixpkgs is specified then load from '<nixpkgs>
    otherwise load from the location speciffied.

--- configuration/machine/common/default.nix
+++ configuration/machine/common/default.nix
@@ -27,6 +27,16 @@
       fdts() { loc=$1; shift; find $loc -type d -printf "%CY-%Cm-%Cd %CH:%CM %p\n" "$@" | sort; }
 
       cdl() { cd $1; ls -l; }
+
+      nixpkgs-repl() {
+        if [ -z "$1" ]; then
+          nixpkgs="<nixpkgs>"
+        else
+          nixpkgs=$1
+        fi
+        echo "Starting repl with nixpkgs from $nixpkgs"
+        nix repl --expr "import ''${nixpkgs} {}"
+      }
     '';
   };
 }
