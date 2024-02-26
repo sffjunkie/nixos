86bf6fc50d916593b6b94a7e4373f1ac174baaea
Simon Kennedy <sffjunkie+code@gmail.com>
2024-02-26 17:25:54 +0000

    Use site configuration to create vlan interfaces

--- configuration/machine/pinky/networking.nix
+++ configuration/machine/pinky/networking.nix
@@ -5,6 +5,28 @@
 }: let
   wanDev = lib.network.netdevice config "pinky" "wan";
   lanDev = lib.network.netdevice config "pinky" "lan";
+
+  vlans = config.looniversity.network.vlans;
+  vlanInterfaces =
+    map
+    (
+      name: let
+        vlanInfo = vlans.${name};
+      in {
+        ${name} = {
+          useDHCP = false;
+          ipv4 = {
+            addresses = [
+              {
+                address = lib.ipv4.constructIpv4Address vlanInfo.network "1";
+                prefixLength = vlanInfo.prefixLength;
+              }
+            ];
+          };
+        };
+      }
+    )
+    (lib.attrNames vlans);
 in {
   config = {
     networking = {
@@ -12,75 +34,26 @@ in {
       hostName = "pinky";
       domain = "looniversity.net";
 
-      interfaces = {
-        "${wanDev}" = {
-          useDHCP = lib.mkDefault false;
-          # ipv4 set by pppd
-        };
-
-        "${lanDev}" = {
-          useDHCP = lib.mkDefault false;
-          ipv4 = {
-            addresses = [
-              {
-                address = "10.44.0.1";
-                prefixLength = 21;
-              }
-            ];
+      interfaces =
+        {
+          "${wanDev}" = {
+            useDHCP = lib.mkDefault false;
+            # ipv4 set by pppd
           };
-        };
 
-        guest = {
-          useDHCP = lib.mkDefault false;
-          ipv4 = {
-            addresses = [
-              {
-                address = "10.44.10.1";
-                prefixLength = 24;
-              }
-            ];
+          "${lanDev}" = {
+            useDHCP = lib.mkDefault false;
+            ipv4 = {
+              addresses = [
+                {
+                  address = "10.44.0.1";
+                  prefixLength = 21;
+                }
+              ];
+            };
           };
-        };
-
-        iot = {
-          useDHCP = lib.mkDefault false;
-          ipv4 = {
-            addresses = [
-              {
-                address = "10.44.20.1";
-                prefixLength = 24;
-              }
-            ];
-          };
-        };
-
-        not = {
-          useDHCP = lib.mkDefault false;
-          ipv4 = {
-            addresses = [
-              {
-                address = "10.44.30.1";
-                prefixLength = 24;
-              }
-            ];
-          };
-        };
-      };
-
-      vlans = {
-        guest = {
-          id = 10;
-          interface = "${lanDev}";
-        };
-        iot = {
-          id = 20;
-          interface = "${lanDev}";
-        };
-        not = {
-          id = 30;
-          interface = "${lanDev}";
-        };
-      };
+        }
+        // vlanInterfaces;
     };
   };
 }
