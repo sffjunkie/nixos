c39836b6ca0b84b59ff308bc47b1cd2ebd6cf5ef
Simon Kennedy <sffjunkie+code@gmail.com>
2024-03-05 12:28:38 +0000

    Add podcaster home-manager role

---
+++ configuration/role/home/podcaster/default.nix
@@ -0,0 +1,16 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.looniversity.role.podcaster;
+  inherit (lib) mkEnableOption mkIf;
+in {
+  options.looniversity.role.podcaster = {
+    enable = mkEnableOption "podcaster role";
+  };
+
+  config = mkIf cfg.enable {
+    programs.obs-studio.enable = treu;
+  };
+}
