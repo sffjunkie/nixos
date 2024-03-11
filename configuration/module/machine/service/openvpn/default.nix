9e328e327378dadfc322ca7ec810a6f4ce0cfd03
Simon Kennedy <sffjunkie+code@gmail.com>
2024-03-11 10:22:28 +0000

    Switch to `services.openvpn`

--- configuration/module/machine/service/openvpn/default.nix
+++ configuration/module/machine/service/openvpn/default.nix
@@ -13,8 +13,27 @@ in {
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = [
-      pkgs.openvpn
-    ];
+    # TODO: Add paths to secrets
+    services.openvpn = {
+      servers = {
+        looniversityVPN = {
+          config = ''
+            client
+            tls-client
+            auth-user-pass
+            dev tun
+            persist-tun
+            persist-key
+            ncp-disable
+            cipher AES-256-CBCauth SHA256
+            resolv-retry infinite
+            remote looniversity.net 1194 udp4
+            nobindverify-x509-name "openvpn" name
+            remote-cert-tls server
+            explicit-exit-notify
+          '';
+        };
+      };
+    };
   };
 }
