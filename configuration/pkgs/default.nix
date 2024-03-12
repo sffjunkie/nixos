2e280a13e34078e411d71d91860f48c2343d5e29
Simon Kennedy <sffjunkie+code@gmail.com>
2024-03-12 11:51:29 +0000

    Change to a simple set of imports

--- configuration/pkgs/default.nix
+++ configuration/pkgs/default.nix
@@ -1,31 +1,5 @@
-{
-  pkgs,
-  lib ? pkgs.lib,
-  debug ? false,
-  ...
-}:
-with lib; let
-  pkgsFolder = ./pkgs;
-  inherit (lib) mapAttrs' removeSuffix filterAttrs;
-in
-  mapAttrs' (name: type: {
-    name = removeSuffix ".nix" name;
-    value = (
-      let
-        file = pkgsFolder + "/${name}";
-      in
-        lib.callPackageWith (pkgs
-          // {
-            inherit debug;
-          })
-        file
-        {}
-    );
-  })
-  (filterAttrs
-    (
-      name: type:
-        (type == "directory" && builtins.pathExists "${toString pkgsFolder}/${name}/default.nix")
-        || (type == "regular" && hasSuffix ".nix" name)
-    )
-    (builtins.readDir pkgsFolder))
+{pkgs, ...}: {
+  environment.systemPackages = [
+    # (pkgs.callPackage ./trino.nix {})
+  ];
+}
