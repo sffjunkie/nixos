b0dd580686691bfff7cf7777eb813b1f711af338
Simon Kennedy <sffjunkie+code@gmail.com>
2024-03-24 13:49:55 +0000

    Switch to default mongodb version

--- configuration/module/machine/service/mongodb/default.nix
+++ configuration/module/machine/service/mongodb/default.nix
@@ -17,8 +17,6 @@ in {
   config = mkIf cfg.enable {
     services.mongodb = {
       enable = true;
-      # Use 5.x as 6 build broken https://github.com/NixOS/nixpkgs/issues/244336
-      package = pkgs.mongodb-5_0;
       # bind_ip = "0.0.0.0";
     };
   };
