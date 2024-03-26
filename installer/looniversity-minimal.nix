a5ae19ab6ab7fc1c4233b3be2648bfd2d5d81611
Simon Kennedy <sffjunkie+code@gmail.com>
2024-03-26 16:50:56 +0000

    Add wpa_supplicant configuration

--- installer/looniversity-minimal.nix
+++ installer/looniversity-minimal.nix
@@ -11,6 +11,19 @@ with lib; {
     sops = {
       defaultSopsFile = ../configuration/secret/secrets.yaml;
       defaultSopsFormat = "yaml";
+
+      secrets."wifi/ssid" = {};
+      secrets."wifi/psk" = {};
+
+      templates."wpa_supplicant".content = ''
+        ctrl_interface=/run/wpa_supplicant
+        ctrl_interface_group=wheel
+        update_config=1
+        network = {
+            ssid=${config.sops.placeholder."wifi/ssid"}
+            psk=${config.sops.placeholder."wifi/psk"}
+        }
+      '';
     };
 
     nix.settings = {
@@ -22,6 +35,8 @@ with lib; {
     environment.etc = {
       nixos.source = ../configuration;
       disko.source = ../disko;
+
+      "wpa_supplicant.conf".source = config.sops.templates."wpa_supplicant".path;
     };
 
     environment.systemPackages = with pkgs; [
