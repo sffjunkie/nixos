0d937657046bc657d5bd878c3bd6267fb843fcce
Simon Kennedy <sffjunkie+code@gmail.com>
2024-03-27 11:15:49 +0000

    Create udev rule package to ensure rules file is before `73-seat-late.rules`

--- configuration/module/machine/system/yubikey/default.nix
+++ configuration/module/machine/system/yubikey/default.nix
@@ -5,6 +5,16 @@
   ...
 }: let
   cfg = config.looniversity.yubikey_plus;
+
+  # https://github.com/systemd/systemd/issues/4288#issuecomment-348166161
+  udev_rule = pkgs.writeTextFile {
+    name = "yubikey_plus_udev";
+    text = ''
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0010", TAG+="uaccess", TAG+="security-device", MODE="0660"
+    '';
+    destination = "/lib/udev/rules.d/71-yubikey_plus.rules";
+  };
+
   inherit (lib) mkEnableOption mkIf;
 in {
   options.looniversity.yubikey_plus = {
@@ -12,8 +22,8 @@ in {
   };
 
   config = mkIf cfg.enable {
-    services.udev.extraRules = ''
-      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0010", TAG+="uaccess"
-    '';
+    services.udev.packages = [
+      udev_rule
+    ];
   };
 }
