7dc965a9a4f345819f2013c61895dc977a891cb4
Simon Kennedy <sffjunkie+code@gmail.com>
2024-03-28 09:08:23 +0000

    Use domain name from config

--- lib/deploy.nix
+++ lib/deploy.nix
@@ -1,9 +1,15 @@
-{lib, ...}: {
+{
+  lib,
+  config,
+  ...
+}: let
+  domaninName = config.looniversity.network.domainName;
+in {
   deployNodes = hosts:
     pkgs.lib.listToAttrs (map (host:
       pkgs.lib.nameValuePair host
       {
-        hostname = "${host}.brage.info";
+        hostname = "${host}.${domaninName}";
         profiles.system = {
           user = "root";
           path = deploy-rs.lib.${system}.activate.nixos self.nixosConfigurations.${host};
