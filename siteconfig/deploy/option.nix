de3930e701e2ce601b47c90c0403e5f6a828004d
Simon Kennedy <sffjunkie+code@gmail.com>
2024-03-28 13:18:01 +0000

    Add deployment options

---
+++ siteconfig/deploy/option.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib) mkOption types;
+in {
+  options.looniversity.deploy = {
+    sshUser = mkOption {
+      type = types.str;
+      default = "";
+    };
+    remoteUser = mkOption {
+      type = types.str;
+      default = "";
+    };
+    targets = mkOption {
+      type = types.listOf str;
+      default = [];
+    };
+  };
+}
