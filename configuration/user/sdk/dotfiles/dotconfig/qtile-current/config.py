d611fefb593ef1f4d654ca84ff57acb76f45f65b
Simon Kennedy <sffjunkie+code@gmail.com>
2024-04-05 14:17:47 +0100

    Remove options not available in swaylock

--- configuration/user/sdk/dotfiles/dotconfig/qtile-current/config.py
+++ configuration/user/sdk/dotfiles/dotconfig/qtile-current/config.py
@@ -80,18 +80,13 @@ def autostart():
     )
     sway_lock = [
         "swaylock",
-        "--screenshots",
         "--clock",
-        "--indicator",
         "--indicator-radius 100",
         "--indicator-thickness 7",
         "--ring-color bb00cc",
         "--key-hl-color 880033",
         "--line-color 00000000",
         "--inside-color 00000088",
-        "--separator-color 00000000",
-        "--grace 10",
-        "--fade-in 0.2",
     ]
     commands = [
         [
