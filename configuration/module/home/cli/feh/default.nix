f41396e0cb551c556c377697ecb9fb879997b7ce
Simon Kennedy <sffjunkie+code@gmail.com>
2024-04-09 08:32:35 +0100

    Move feh configuration into nix module

--- configuration/module/home/cli/feh/default.nix
+++ configuration/module/home/cli/feh/default.nix
@@ -5,6 +5,21 @@
   ...
 }: let
   cfg = config.looniversity.feh;
+
+  buttonsFile = ''
+    # Unbind existing scroll operations
+    prev_img
+    next_img
+    # Set <action> <mouse button>
+    zoom_in 4
+    zoom_out 5
+  '';
+
+  themesFile = ''
+    default -Gd -B black --draw-tinted --scale-down -g "2880x1620+480+270" --font "Roboto-Regular/13" --fontpath ${pkgs.roboto}/share/fonts/truetype/ --info 'echo "Size: %wx%h, Format: %t, Pixels: %P"'
+    slideshow -G -B black --draw-tinted --fullscreen --font "Roboto-Regular/13" --fontpath ${pkgs.roboto}/share/fonts/truetype/ --slideshow-delay 3 --info ';echo "Size: %wx%h, Format: %t, Pixels: %P"'
+  '';
+
   inherit (lib) mkEnableOption mkIf;
 in {
   options.looniversity.feh = {
@@ -16,8 +31,8 @@ in {
       pkgs.feh
     ];
 
-    xdg.configFile."feh/buttons".source = ./buttons;
-    xdg.configFile."feh/themes".source = ./themes;
+    xdg.configFile."feh/buttons".text = buttonsFile;
+    xdg.configFile."feh/themes".text = themesFile;
 
     programs.zsh.shellAliases = {
       feh = "feh -Tdefault";
