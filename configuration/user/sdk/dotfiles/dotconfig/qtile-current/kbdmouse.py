536ff34c7bbeea38f8afdb20b231d1e403366a7e
Simon Kennedy <sffjunkie+code@gmail.com>
2024-04-11 12:31:10 +0100

    Modify key bindings

--- configuration/user/sdk/dotfiles/dotconfig/qtile-current/kbdmouse.py
+++ configuration/user/sdk/dotfiles/dotconfig/qtile-current/kbdmouse.py
@@ -1,25 +1,52 @@
 # Keybindings for groups are defined in groups.py
 
+import shlex
 from os import environ
 from typing import List  # noqa: F401
 
 # from libqtile import extension
 from libqtile.command import lazy
-from libqtile.config import Key, Drag, Click
+from libqtile.config import Click, Drag, Key
 
 Alt = "mod1"
+Super = "mod4"
+ApplicationLaunchModKey = Alt
 
 
 def build_keys(settings) -> List[Key]:
+    sway_lock = [
+        "swaylock",
+        "--color=3B4252",
+        "--ring-color=D8DEE9",
+        "--inside-color=4C566A",
+        "--indicator-radius=100",
+        "--indicator-thickness=10",
+        "--key-hl-color=880033",
+        "--line-color=5E81AC",
+    ]
+
     return [
+        # Browser
         Key(
-            [settings["mod"], "shift"],
+            [Super, ApplicationLaunchModKey],
+            "b",
+            lazy.spawn(environ.get("BROWSER", "brave")),
+        ),
+        # Code Editor
+        Key(
+            [Super, ApplicationLaunchModKey],
+            "c",
+            lazy.spawn("code"),
+        ),
+        # Terminal
+        Key(
+            [Super, ApplicationLaunchModKey],
             "t",
             lazy.spawn(environ.get("TERMINAL", "alacritty")),
         ),
         # App launcher
         Key(
-            [settings["mod"]],
+            [Super, ApplicationLaunchModKey],
             "Space",
             lazy.spawn(
                 "rofi -modi drun -show drun -theme-str '@import \"looniversity\"'"
@@ -27,13 +54,13 @@ def build_keys(settings) -> List[Key]:
         ),
         # Lock screen
         Key(
-            [settings["mod"], Alt],
+            [Super, ApplicationLaunchModKey],
             "l",
-            lazy.spawn("swaylock -f -c 000000"),
+            lazy.spawn(shlex.join(sway_lock)),
         ),
         # Power menu
         Key(
-            [settings["mod"], Alt],
+            [Super, ApplicationLaunchModKey],
             "F12",
             lazy.spawn(
                 "rofi -show p -modi p:rofi-power-menu -theme-str '@import \"looniversity\"'"
@@ -42,139 +69,137 @@ def build_keys(settings) -> List[Key]:
         # region MPD Control
         # Play / Pause
         Key(
-            [settings["mod"]],
+            [Super],
             "F8",
             lazy.spawn("musicctl toggle"),
         ),
         Key(
-            [settings["mod"]],
+            [Super],
             "F7",
             lazy.spawn("musicctl previous"),
         ),
         Key(
-            [settings["mod"]],
+            [Super],
             "F9",
             lazy.spawn("musicctl next"),
         ),
         # endregion
         # region QTile Control
-        Key([settings["mod"], Alt], "r", lazy.reload_config(), desc="Reload QTile"),
-        Key([settings["mod"], Alt], "q", lazy.shutdown(), desc="Quit QTile"),
+        Key([Super, Alt], "r", lazy.reload_config(), desc="Reload QTile"),
+        Key([Super, Alt], "q", lazy.shutdown(), desc="Quit QTile"),
         # endregion
         # region Window Control
         Key(
-            [settings["mod"], "shift"],
+            [Super, "shift"],
             "c",
             lazy.window.kill(),
             desc="Close window",
         ),
         Key(
-            [settings["mod"]],
+            [Super],
             "Delete",
             lazy.window.kill(),
             desc="Close window",
         ),
         Key(
-            [settings["mod"], "mod1"],
+            [Super, "mod1"],
             "Left",
             lazy.screen.prev_group(),
             desc="Switch to next group",
         ),
         Key(
-            [settings["mod"], "mod1"],
+            [Super, "mod1"],
             "Right",
             lazy.screen.next_group(),
             desc="Switch to previous group",
         ),
         Key(
-            [settings["mod"], Alt],
+            [Super, Alt],
             "f",
             lazy.window.toggle_floating(),
             desc="Toggle floating window",
         ),
         # endregion
         # region Toggle between different layouts as defined below
-        Key(
-            [settings["mod"]], "grave", lazy.next_layout(), desc="Switch to next layout"
-        ),
+        Key([Super], "grave", lazy.next_layout(), desc="Switch to next layout"),
         # endregion
         # region Move window in stack
         Key(
-            [settings["mod"], "shift"],
+            [Super, "shift"],
             "Right",
             lazy.layout.shuffle_down(),
             desc="Move window down in stack",
         ),
         Key(
-            [settings["mod"], "shift"],
+            [Super, "shift"],
             "Left",
             lazy.layout.shuffle_up(),
             desc="Move window up in stack",
         ),
         Key(
-            [settings["mod"], "shift"],
+            [Super, "shift"],
             "l",
             lazy.layout.shuffle_down(),
             desc="Move window down in stack",
         ),
         Key(
-            [settings["mod"], "shift"],
+            [Super, "shift"],
             "h",
             lazy.layout.shuffle_up(),
             desc="Move window up in stack",
         ),
         # endregion
         # region Switch between windows in current stack pane
-        Key([settings["mod"]], "h", lazy.layout.up(), desc="Previous window"),
-        Key([settings["mod"]], "l", lazy.layout.down(), desc="Next window"),
-        Key([settings["mod"]], "Left", lazy.layout.up(), desc="Previous window"),
-        Key([settings["mod"]], "Right", lazy.layout.down(), desc="Next window"),
+        Key([Super], "h", lazy.layout.up(), desc="Previous window"),
+        Key([Super], "l", lazy.layout.down(), desc="Next window"),
+        Key([Super], "Left", lazy.layout.up(), desc="Previous window"),
+        Key([Super], "Right", lazy.layout.down(), desc="Next window"),
         # endregion
         # region Resize
         Key(
-            [settings["mod"], "control"],
+            [Super, "control"],
             "Right",
             lazy.layout.grow_main(),
             desc="Increase Main Window Size",
         ),
         Key(
-            [settings["mod"], "control"],
+            [Super, "control"],
             "l",
             lazy.layout.grow_main(),
             desc="Increase Main Window Size",
         ),
         Key(
-            [settings["mod"], "control"],
+            [Super, "control"],
             "Left",
             lazy.layout.shrink_main(),
             desc="Decrease Main Window Size",
         ),
         Key(
-            [settings["mod"], "control"],
+            [Super, "control"],
             "h",
             lazy.layout.shrink_main(),
             desc="Decrease Main Window Size",
         ),
         Key(
-            [settings["mod"], "control"],
+            [Super, "control"],
             "Up",
             lazy.layout.grow(),
             desc="Increase Sub Window Size",
         ),
         Key(
-            [settings["mod"], "control"],
+            [Super, "control"],
             "j",
             lazy.layout.grow(),
             desc="Increase Sub Window Size",
         ),
         Key(
-            [settings["mod"], "control"],
+            [Super, "control"],
             "Down",
             lazy.layout.shrink(),
             desc="Decrease Sub Window Size",
         ),
         Key(
-            [settings["mod"], "control"],
+            [Super, "control"],
             "k",
             lazy.layout.shrink(),
             desc="Decrease Sub Window Size",
@@ -195,16 +220,16 @@ def build_keys(settings) -> List[Key]:
 def bind_mouse_buttons(settings):
     return [
         Drag(
-            [settings["mod"]],
+            [Super],
             "Button1",
             lazy.window.set_position_floating(),
             start=lazy.window.get_position(),
         ),
         Drag(
-            [settings["mod"]],
+            [Super],
             "Button3",
             lazy.window.set_size_floating(),
             start=lazy.window.get_size(),
         ),
-        Click([settings["mod"]], "Button2", lazy.window.bring_to_front()),
+        Click([Super], "Button2", lazy.window.bring_to_front()),
     ]
