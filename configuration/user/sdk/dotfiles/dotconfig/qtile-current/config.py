2548a659cd7dd7a5b7e148af36299e4e679290aa
Simon Kennedy <sffjunkie+code@gmail.com>
2024-04-11 12:30:30 +0100

    Update swaylock command line

--- configuration/user/sdk/dotfiles/dotconfig/qtile-current/config.py
+++ configuration/user/sdk/dotfiles/dotconfig/qtile-current/config.py
@@ -105,12 +105,12 @@ def start():
     sway_lock = [
         "swaylock",
         "--clock",
-        "--indicator-radius 100",
-        "--indicator-thickness 7",
-        "--ring-color bb00cc",
-        "--key-hl-color 880033",
-        "--line-color 00000000",
-        "--inside-color 00000088",
+        "--indicator-radius=100",
+        "--indicator-thickness=7",
+        "--ring-color=bb00cc",
+        "--key-hl-color=880033",
+        "--line-color=00000000",
+        "--inside-color=00000088",
     ]
     commands = [
         [
