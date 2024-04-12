c7e17257d2ddc274e37f9898866ee4eb1efd5063
Simon Kennedy <sffjunkie+code@gmail.com>
2024-04-12 13:13:02 +0100

    Reorganise rules

--- configuration/user/sdk/home/settings/qtile/qtile-current/rule.py
+++ configuration/user/sdk/home/settings/qtile/qtile-current/rule.py
@@ -4,6 +4,23 @@ from typing import List
 
 from libqtile.config import Match, Rule
 
+wmclass_group = {
+    "brave-browser|chromium|firefox": "WWW",
+    "obsidian": "BRAIN",
+    "code-url-handler": "DEV",
+    "Darktable": "GFX",
+    "Gimp": "GFX",
+    "discord": "CHAT",
+}
+
+
+def dgroup_rules() -> List[Rule]:
+    return [
+        Rule(Match(wm_class=wmclass), group=group)
+        for wmclass, group in wmclass_group.items()
+    ]
+
+
 wmclass_float = [
     "Pavucontrol",
     "Volumeicon",
@@ -12,28 +29,8 @@ wmclass_float = [
     "ssh-askpass",
 ]
 
-wmclass_group = {
-    "brave-browser|chromium|firefox": "WWW",
-    "code-url-handler": "DEV",
-    "Darktable": "GFX",
-    "Gimp": "GFX",
-    "discord": "CHAT",
-}
 
-
-def build_rules() -> List[Rule]:
+def float_rules() -> List[Rule]:
     float_escaped = [re.escape(item) for item in wmclass_float]
     float_match = "|".join(float_escaped)
-
-    rules = [
-        Rule(
-            Match(wm_class=f"^({float_match})$"),
-            float=True,
-            break_on_match=True,
-        ),
-    ]
-
-    for wmclass, group in wmclass_group.items():
-        rules.append(Rule(Match(wm_class=wmclass), group=group))
-
-    return rules
+    return [Match(wm_class=f"^({float_match})$")]
