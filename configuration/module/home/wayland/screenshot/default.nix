f566117e802703985b92154d9d55d25ced036c75
Simon Kennedy <sffjunkie+code@gmail.com>
2024-04-13 08:22:29 +0100

    Update sshot script and add swappy config

--- configuration/module/home/wayland/screenshot/default.nix
+++ configuration/module/home/wayland/screenshot/default.nix
@@ -6,8 +6,71 @@
 }: let
   cfg = config.looniversity.wayland.screenshot;
 
-  sshot = pkgs.writeShellScriptBin ''
-    ${pkgs.grim}/bin/grim -g "''$(${pkgs.slurp}/bin/slurp)"
+  sshot = pkgs.writeScriptBin "sshot" ''
+    #!${lib.getExe pkgs.bash}
+
+    help() {
+      echo "sshot - wayland screenshot script"
+      echo
+      echo "Usage:  sshot [options]"
+      echo "  -e, --edit      send screenshot to swappy"
+      echo "  -r, --region    screenshot a region"
+      echo "  -v, --verbose   display information about actions taken"
+      echo
+      echo "If '-e' not specified then the output will be sent to the clipboard"
+      echo "If '-r' not specified the entire screen will be captured"
+    }
+
+    VALID_ARGS=$(getopt -o herv --long help,edit,region,verbose -- "''$@")
+    if [[ $? -ne 0 ]]; then
+      help
+      exit 0;
+    fi
+
+    output=""
+    region=0
+    eval set -- "$VALID_ARGS"
+    while [ : ]; do
+      case "$1" in
+        -h | --help)
+          help
+          exit 0
+          ;;
+        -e | --edit)
+          output="swappy"
+          shift
+          ;;
+        -r | --region)
+          region=1
+          shift
+          ;;
+        -v | --verbose)
+          verbose=1
+          shift
+          ;;
+        --)
+          break
+          ;;
+      esac
+    done
+
+    if [ $region -eq 1 ]; then
+      if [ "$output" == "swappy" ]; then
+        [ $verbose -eq 1 ] && echo "Saving screenshot to $screenshot_dir"
+        ${pkgs.grim}/bin/grim -t png -g "''$(${pkgs.slurp}/bin/slurp)" - | ${pkgs.swappy}/bin/swappy -f -
+      else
+        ${pkgs.grim}/bin/grim -t png -g "''$(${pkgs.slurp}/bin/slurp)" - | ${pkgs.wl-clipboard}/bin/wl-copy
+        [ $? -eq 0 ] && [ $verbose -eq 1 ] && echo "Screenshot copied to clipboard"
+      fi
+    else
+      if [ "$output" == "swappy" ]; then
+        [ $verbose -eq 1 ] && echo "Saving screenshot to $screenshot_dir"
+        ${pkgs.grim}/bin/grim -t png - | ${pkgs.swappy}/bin/swappy -f -
+      else
+        ${pkgs.grim}/bin/grim -t png - | ${pkgs.wl-clipboard}/bin/wl-copy
+        [ $? -eq 0 ] && [ $verbose -eq 1 ] && echo "Screenshot copied to clipboard"
+      fi
+    fi
   '';
 
   inherit (lib) mkDefault mkEnableOption mkIf mkOption;
@@ -18,5 +81,11 @@ in {
     home.packages = [
       sshot
     ];
+
+    xdg.configFile."swappy/config".text = ''
+      [Default]
+      save_dir=$HOME/pictures/screenshots
+      save_filename_format=swappy-%Y%m%d-%H%M%S.png
+    '';
   };
 }
