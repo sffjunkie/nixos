77d873b7205ba3019667d515d55f4c40e333afc9
Simon Kennedy <sffjunkie+code@gmail.com>
2024-04-13 17:54:58 +0100

    Configure swaylock/swayidle

--- configuration/module/home/desktop/swaylock/default.nix
+++ configuration/module/home/desktop/swaylock/default.nix
@@ -12,7 +12,33 @@ in {
   };
 
   config = mkIf cfg.enable {
-    programs.swaylock.enable = true;
-    services.swayidle.enable = true;
+    programs.swaylock = {
+      enable = true;
+      # style set by stylix
+    };
+
+    services.swayidle = {
+      enable = true;
+      timeouts = [
+        {
+          timeout = 180;
+          command = "${pkgs.swaylock}/bin/swaylock -fF";
+        }
+        {
+          timeout = 300;
+          command = "${pkgs.systemd}/bin/systemctl suspend";
+        }
+      ];
+      events = [
+        {
+          event = "before-sleep";
+          command = "${pkgs.swaylock}/bin/swaylock -fF";
+        }
+        {
+          event = "lock";
+          command = "${pkgs.swaylock}/bin/swaylock -fF";
+        }
+      ];
+    };
   };
 }
