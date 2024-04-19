c8173d571d0bb9af2b6161e8b3509652ced3f2bc
Simon Kennedy <sffjunkie+code@gmail.com>
2024-04-19 09:24:25 +0100

    Add window management functions

---
+++ configuration/user/sdk/home/settings/qtile/config/window.py
@@ -0,0 +1,9 @@
+from libqtile.command import lazy
+
+
+@lazy.function
+def float_to_front(qtile):
+    for group in qtile.groups:
+        for window in group.windows:
+            if window.floating:
+                window.cmd_bring_to_front()
