58475e9ddc1e47d8339b2c9dde3802d62d54b2f3
Simon Kennedy <sffjunkie+code@gmail.com>
2024-05-19 07:50:22 +0100

    Add options

--- configuration/module/home/editor/nixvim/default.nix
+++ configuration/module/home/editor/nixvim/default.nix
@@ -21,6 +21,30 @@ in {
         have_nerd_font = true;
       };
 
+      opts = {
+        number = true;
+        mouse = "a";
+        showmode = false;
+        clipboard = "unnamedplus";
+        breakindent = true;
+        undofile = true;
+        ignorecase = true;
+        smartcase = true;
+        signcolumn = "yes";
+        updatetime = 250;
+        timeoutlen = 300;
+        splitright = true;
+        splitbelow = true;
+        list = true;
+        listchars = {
+          tab = "» ";
+          trail = "·";
+          nbsp = "␣";
+        };
+        inccommand = "split";
+        cursorline = true;
+        scrolloff = 10;
+      };
 
       plugins = {
         lazy.enable = true;
