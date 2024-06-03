c2e2a750360d6d585cbbb37cc8b024c7d535d340
Simon Kennedy <sffjunkie+code@gmail.com>
2024-06-03 10:28:33 +0100

    Update fd function name to not shadow fd command. change ff name to use same naming scheme

--- configuration/machine/common/default.nix
+++ configuration/machine/common/default.nix
@@ -29,10 +29,10 @@
     };
 
     environment.interactiveShellInit = ''
-      ff() { loc=$1; shift; find $loc -type f "$@"; }
+      ffd() { loc=$1; shift; find $loc -type f "$@"; }
       fft() { loc=$1; shift; find $loc -type f -printf "%CY-%Cm-%Cd %CH:%CM %p\n" "$@"; }
       ffts() { loc=$1; shift; find $loc -type f -printf "%CY-%Cm-%Cd %CH:%CM %p\n" "$@" | sort; }
-      fd() { loc=$1; shift; find $loc -type d "$@"; }
+      fdd() { loc=$1; shift; find $loc -type d "$@"; }
       fdt() { loc=$1; shift; find $loc -type d -printf "%CY-%Cm-%Cd %CH:%CM %p\n" "$@"; }
       fdts() { loc=$1; shift; find $loc -type d -printf "%CY-%Cm-%Cd %CH:%CM %p\n" "$@" | sort; }
 
