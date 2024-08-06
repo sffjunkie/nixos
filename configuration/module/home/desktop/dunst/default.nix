ce4e2fc3db5b1095aa08754e6e77985826f3ab60
Simon Kennedy <sffjunkie+code@gmail.com>
2024-08-06 17:47:39 +0100

    Set notification font

--- configuration/module/home/desktop/dunst/default.nix
+++ configuration/module/home/desktop/dunst/default.nix
@@ -17,6 +17,7 @@ in {
       waylandDisplay = "wayland-0";
       settings = {
         global = {
+          font = "Hack Nerd Font 13";
           width = 500;
           height = 300;
           origin = "top-right";
