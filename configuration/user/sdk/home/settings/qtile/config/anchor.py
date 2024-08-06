6b71478c9dbb3b80e1d2e19abcd746e0891e8ba7
Simon Kennedy <sffjunkie+code@gmail.com>
2024-08-06 11:09:11 +0100

    Add margin support

--- configuration/user/sdk/home/settings/qtile/config/anchor.py
+++ configuration/user/sdk/home/settings/qtile/config/anchor.py
@@ -12,6 +12,13 @@ class FloatRange:
 ScreenFraction = Annotated[float, FloatRange(min=0.0, max=1.0)]
 
 
+Margin = (
+    ScreenFraction
+    | tuple[ScreenFraction, ScreenFraction]
+    | tuple[ScreenFraction, ScreenFraction, ScreenFraction, ScreenFraction]
+)
+
+
 @dataclass
 class WindowPosition:
     x: float
@@ -34,78 +41,94 @@ class WindowLocation(Enum):
     Centered = auto()
 
 
+def clamp(value: ScreenFraction) -> ScreenFraction:
+    if value < 0.0:
+        return 0.0
+    elif value > 1.0:
+        return 1.0
+
+    return value
+
+
 def anchor_window(
     location: WindowLocation,
     width: ScreenFraction,
     height: ScreenFraction,
+    margin: Margin = 0.0,
 ) -> WindowPosition:
+    if width + (2.0 * margin) > 1.0:
+        width = 1.0 - (2.0 * margin)
+
+    if height + 2.0 * margin > 1.0:
+        height = 1.0 - (2.0 * margin)
+
     if location == WindowLocation.Left:
         return WindowPosition(
-            x=0.0,
-            y=width,
+            x=0.0 + margin,
+            y=0.0 + margin,
             width=width,
-            height=1.0,
+            height=1.0 - (2.0 * margin),
         )
     elif location == WindowLocation.Right:
         return WindowPosition(
-            x=1.0 - width,
-            y=0.0,
+            x=1.0 - width - margin,
+            y=0.0 + margin,
             width=width,
-            height=1.0,
+            height=1.0 - (2.0 * margin),
         )
     elif location == WindowLocation.Top:
         return WindowPosition(
-            x=0.0,
-            y=0.0,
-            width=1.0,
+            x=0.0 + margin,
+            y=0.0 + margin,
+            width=1.0 - (2.0 * margin),
             height=height,
         )
     elif location == WindowLocation.Bottom:
         return WindowPosition(
-            x=0.0,
-            y=1.0 - height,
+            x=0.0 + margin,
+            y=1.0 - height - margin,
             width=1.0,
             height=height,
         )
     elif location == WindowLocation.TopLeft:
         return WindowPosition(
-            x=0.0,
-            y=0.0,
+            x=0.0 + margin,
+            y=0.0 + margin,
             width=width,
             height=height,
         )
     elif location == WindowLocation.TopCenter:
         return WindowPosition(
             x=(1.0 - width) / 2.0,
-            y=0.0,
+            y=0.0 + margin,
             width=width,
             height=height,
         )
     elif location == WindowLocation.TopRight:
         return WindowPosition(
-            x=1.0 - width,
-            y=0.0,
+            x=1.0 - width - margin,
+            y=0.0 + margin,
             width=width,
             height=height,
         )
     elif location == WindowLocation.BottomLeft:
         return WindowPosition(
-            x=0.0,
-            y=1.0 - height,
+            x=0.0 + margin,
+            y=1.0 - height - margin,
             width=width,
             height=height,
         )
     elif location == WindowLocation.BottomCenter:
         return WindowPosition(
             x=(1.0 - width) / 2.0,
-            y=1.0 - height,
+            y=1.0 - height - margin,
             width=width,
             height=height,
         )
     elif location == WindowLocation.BottomRight:
         return WindowPosition(
-            x=1.0 - width,
-            y=1.0 - height,
+            x=1.0 - width - margin,
+            y=1.0 - height - margin,
             width=width,
             height=height,
         )
@@ -116,5 +139,3 @@ def anchor_window(
             width=width,
             height=height,
         )
-
-    raise ValueError("Unkown window position")
