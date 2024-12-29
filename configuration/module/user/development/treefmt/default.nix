2e401160b8bfcd5500d95280f24dd5c641348754
Simon Kennedy <sffjunkie+code@gmail.com>
2024-12-29 13:23:06 +0000

    Add treefmt module

---
+++ configuration/module/user/development/treefmt/default.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.looniversity.development.treefmt;
+  inherit (lib) mkEnableOption mkIf;
+in {
+  options.looniversity.development.treefmt = {
+    enable = mkEnableOption "treefmt";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [
+      pkgs.treefmt2
+    ];
+  };
+}
