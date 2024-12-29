c9669d9dd19e73b69e1c5d67daba33d1974746e2
Simon Kennedy <sffjunkie+code@gmail.com>
2024-12-29 10:52:20 +0000

    Set shell to zsh

--- configuration/user/sysadmin/machine/default.nix
+++ configuration/user/sysadmin/machine/default.nix
@@ -1,4 +1,8 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   config = {
     # https://github.com/Mic92/sops-nix#setting-a-users-password
     sops.secrets."sysadmin/password_hash" = {
@@ -11,6 +15,7 @@
       uid = 1000;
       description = "System Administrator";
       extraGroups = ["networkmanager" "wheel" "docker" "podman"];
+      shell = pkgs.zsh;
       openssh = {
         authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO17K8Ei9367OcAQtB/u/LXb9elGRGJh0p4S9n6DrBy9 sysadmin@furrball"
