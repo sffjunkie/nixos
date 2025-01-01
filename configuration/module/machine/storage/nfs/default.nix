b26f8f7198f84de054e72299d39afab9ce749752
Simon Kennedy <sffjunkie+code@gmail.com>
2025-01-01 08:24:24 +0000

    Moved exports definition to `fs.exports` modukle

--- configuration/module/machine/storage/nfs/default.nix
+++ configuration/module/machine/storage/nfs/default.nix
@@ -1,51 +1,46 @@
-{ config
-, lib
-, pkgs
-, ...
+{
+  config,
+  lib,
+  pkgs,
+  ...
 }:
 let
-  cfg = config.looniversity.storage.nfs;
-  inherit (lib) mkEnableOption mkIf mkOption types;
+  inherit (lib) mkIf mkOption types;
 in
 {
-  options.looniversity.storage.nfs = {
-    enable = mkEnableOption "nfs";
+  config =
+    mkIf config.looniversity.fs.nfs.exports != [ ] {
+      services.nfs = {
+        server = {
+          enable = true;
+          statdPort = 4000;
+          lockdPort = 4001;
+          mountdPort = 4002;
+        };
 
-    exports = mkOption {
-      type = types.listOf types.str;
-      default = [ ];
-      description = lib.mdDoc ''
-        A list of NFS exports
-      '';
-    };
-  };
-
-  config = mkIf cfg.enable {
-    services.nfs = {
-      server = {
-        enable = true;
-        exports =
-          lib.concatMapStringsSep
-            "\n"
-            (share: "${share} ${config.looniversity.network.networkAddress}/${toString config.looniversity.network.prefixLength}(rw,no_subtree_check)")
-            config.looniversity.storage.nfs.exports;
-
-        statdPort = 4000;
-        lockdPort = 4001;
-        mountdPort = 4002;
+        settings = {
+          nfsd.rdma = true; # Remote Direct Memory Access
+          nfsd.vers3 = false;
+          nfsd.vers4 = true;
+          nfsd."vers4.0" = false;
+          nfsd."vers4.1" = false;
+          nfsd."vers4.2" = true;
+        };
       };
 
-      settings = {
-        nfsd.rdma = true; # Remote Direct Memory Access
-        nfsd.vers3 = false;
-        nfsd.vers4 = true;
-        nfsd."vers4.0" = false;
-        nfsd."vers4.1" = false;
-        nfsd."vers4.2" = true;
-      };
+      networking.firewall.allowedTCPPorts = [
+        111
+        2049
+        4000
+        4001
+        4002
+      ];
+      networking.firewall.allowedUDPPorts = [
+        111
+        2049
+        4000
+        4001
+        4002
+      ];
     };
-
-    networking.firewall.allowedTCPPorts = [ 111 2049 4000 4001 4002 ];
-    networking.firewall.allowedUDPPorts = [ 111 2049 4000 4001 4002 ];
-  };
 }
