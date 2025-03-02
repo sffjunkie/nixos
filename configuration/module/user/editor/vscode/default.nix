a24a569e1aeb2f66d6841a36a57a10a681a33a0e
Simon Kennedy <sffjunkie+code@gmail.com>
2025-03-02 18:51:03 +0000

    Move settings into `profiles.default`

--- configuration/module/user/editor/vscode/default.nix
+++ configuration/module/user/editor/vscode/default.nix
@@ -72,88 +72,92 @@ in
       package = pkgs.vscode.fhs;
 
       mutableExtensionsDir = true;
-      extensions = vscodeExtensions ++ marketplaceExtensionsList;
+      profiles = {
+        default = {
+          extensions = vscodeExtensions ++ marketplaceExtensionsList;
 
-      userSettings = {
-        "cSpell.enabled" = false;
+          userSettings = {
+            "cSpell.enabled" = false;
 
-        "diffEditor.ignoreTrimWhitespace" = false;
+            "diffEditor.ignoreTrimWhitespace" = false;
 
-        "editor.fontFamily" = mkIf (
-          !config.stylix.targets.vscode.enable
-        ) "'JetBrainsMono Nerd Font', 'DejaVu Sans Mono', monospace";
-        "editor.fontSize" = mkIf (!config.stylix.targets.vscode.enable) 12;
-        "editor.tokenColorCustomizations" = {
-          "[Nord]" = {
-            "comments" = "#94b9a6";
+            "editor.fontFamily" = mkIf (
+              !config.stylix.targets.vscode.enable
+            ) "'JetBrainsMono Nerd Font', 'DejaVu Sans Mono', monospace";
+            "editor.fontSize" = mkIf (!config.stylix.targets.vscode.enable) 12;
+            "editor.tokenColorCustomizations" = {
+              "[Nord]" = {
+                "comments" = "#94b9a6";
+              };
+            };
+            "editor.formatOnSave" = true;
+            "editor.lineNumbers" = "relative";
+            "editor.parameterHints.enabled" = false;
+
+            "editor.rulers" = [
+              80
+              100
+            ];
+
+            "explorer.confirmDragAndDrop" = false;
+            "explorer.compactFolders" = false;
+
+            "files.associations" = {
+              "*.just" = "just";
+            };
+            "files.insertFinalNewline" = true;
+            "files.exclude" = {
+              "**/.tox" = true;
+              ".devenv" = true;
+              ".direnv" = true;
+            };
+
+            "git.confirmSync" = false;
+            "git.openRepositoryInParentFolders" = "never";
+
+            "markdown.preview.fontSize" = mkIf (!config.stylix.targets.vscode.enable) 12;
+            "markdown.preview.fontFamily" = mkIf (
+              !config.stylix.targets.vscode.enable
+            ) "'JetBrainsMono Nerd Font', 'DejaVu Sans Mono', monospace";
+
+            "python.analysis.diagnosticMode" = "workspace";
+            "python.analysis.exclude" = [
+              "**/result"
+              "**/.devenv"
+              "**/.direnv"
+            ];
+
+            "terminal.integrated.scrollback" = 5000;
+            "terminal.integrated.fontSize" = mkIf (!config.stylix.targets.vscode.enable) 12;
+            "terminal.integrated.defaultProfile.linux" = "bash";
+
+            "update.mode" = "manual";
+
+            "window.titleBarStyle" = "custom";
+            "window.zoomLevel" = mkDefault 2;
+
+            "workbench.colorTheme" = mkIf (!config.stylix.targets.vscode.enable) "Catppuccin Macchiato";
+            "workbench.panel.defaultLocation" = "right";
+            "workbench.startupEditor" = "readme";
+
+            "[markdown]" = {
+              "editor.defaultFormatter" = "yzhang.markdown-all-in-one";
+            };
+            "[nix]" = {
+              "editor.tabSize" = 2;
+              "editor.defaultFormatter" = "brettm12345.nixfmt-vscode";
+            };
+            "[python]" = {
+              "editor.defaultFormatter" = "charliermarsh.ruff";
+            };
+            "[jsonc]" = {
+              "editor.defaultFormatter" = "vscode.json-language-features";
+            };
+
+            "evenBetterToml.formatter.arrayTrailingComma" = true;
+            "evenBetterToml.formatter.arrayAutoExpand" = true;
           };
         };
-        "editor.formatOnSave" = true;
-        "editor.lineNumbers" = "relative";
-        "editor.parameterHints.enabled" = false;
-
-        "editor.rulers" = [
-          80
-          100
-        ];
-
-        "explorer.confirmDragAndDrop" = false;
-        "explorer.compactFolders" = false;
-
-        "files.associations" = {
-          "*.just" = "just";
-        };
-        "files.insertFinalNewline" = true;
-        "files.exclude" = {
-          "**/.tox" = true;
-          ".devenv" = true;
-          ".direnv" = true;
-        };
-
-        "git.confirmSync" = false;
-        "git.openRepositoryInParentFolders" = "never";
-
-        "markdown.preview.fontSize" = mkIf (!config.stylix.targets.vscode.enable) 12;
-        "markdown.preview.fontFamily" = mkIf (
-          !config.stylix.targets.vscode.enable
-        ) "'JetBrainsMono Nerd Font', 'DejaVu Sans Mono', monospace";
-
-        "python.analysis.diagnosticMode" = "workspace";
-        "python.analysis.exclude" = [
-          "**/result"
-          "**/.devenv"
-          "**/.direnv"
-        ];
-
-        "terminal.integrated.scrollback" = 5000;
-        "terminal.integrated.fontSize" = mkIf (!config.stylix.targets.vscode.enable) 12;
-        "terminal.integrated.defaultProfile.linux" = "bash";
-
-        "update.mode" = "manual";
-
-        "window.titleBarStyle" = "custom";
-        "window.zoomLevel" = mkDefault 2;
-
-        "workbench.colorTheme" = mkIf (!config.stylix.targets.vscode.enable) "Catppuccin Macchiato";
-        "workbench.panel.defaultLocation" = "right";
-        "workbench.startupEditor" = "readme";
-
-        "[markdown]" = {
-          "editor.defaultFormatter" = "yzhang.markdown-all-in-one";
-        };
-        "[nix]" = {
-          "editor.tabSize" = 2;
-          "editor.defaultFormatter" = "brettm12345.nixfmt-vscode";
-        };
-        "[python]" = {
-          "editor.defaultFormatter" = "charliermarsh.ruff";
-        };
-        "[jsonc]" = {
-          "editor.defaultFormatter" = "vscode.json-language-features";
-        };
-
-        "evenBetterToml.formatter.arrayTrailingComma" = true;
-        "evenBetterToml.formatter.arrayAutoExpand" = true;
       };
     };
   };
