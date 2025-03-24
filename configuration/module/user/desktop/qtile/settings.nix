c4e62ced20cd1c6489c15bc81549aafcdf94354d
Simon Kennedy <sffjunkie+code@gmail.com>
2025-03-24 10:17:12 +0000

    Update qtile config files

--- configuration/module/user/desktop/qtile/settings.nix
+++ configuration/module/user/desktop/qtile/settings.nix
@@ -7,28 +7,20 @@
 {
   config = {
     xdg.configFile."desktop/default_settings.yaml".text = lib.generators.toYAML { } {
-      apps = {
+      app = {
         terminal = "xterm";
-        volume = "pavucontrol";
       };
-      keys = {
-        Alt = "mod1";
-        Ctrl = "control";
-        Hyper = "mod3";
-        Shift = "shift";
-        Super = "mod4";
+
+      device = {
+        net = "eth0";
       };
-      commands = {
-        music = {
-          play = "musicctl play";
-          next = "musicctl next";
-        };
-        volume = {
-          up = "volumectl up";
-          down = "volumectl down";
-          mute = "volumectl mute";
-          toggle = "volumectl toggle";
-        };
+
+      key = {
+        alt = "mod1";
+        ctrl = "control";
+        hyper = "mod3";
+        shift = "shift";
+        cmd = "mod4";
       };
     };
   };
--- configuration/module/user/desktop/qtile/theme.nix
+++ configuration/module/user/desktop/qtile/theme.nix
@@ -26,79 +26,90 @@ let
     size = defaultFontSize;
   };
 
-  defaultBarHeight = 22;
-  defaultBarOpacity = 0.8;
+  defaultMargin = 4;
 
   defaultBase16Colors = {
-    base00 = "f9f5d7";
-    base01 = "ebdbb2";
-    base02 = "d5c4a1";
-    base03 = "bdae93";
-    base04 = "665c54";
-    base05 = "504945";
-    base06 = "3c3836";
-    base07 = "282828";
-    base08 = "9d0006";
-    base09 = "af3a03";
-    base0A = "b57614";
-    base0B = "79740e";
-    base0C = "427b58";
-    base0D = "076678";
-    base0E = "8f3f71";
-    base0F = "d65d0e";
+    base00 = "#f9f5d7";
+    base01 = "#ebdbb2";
+    base02 = "#d5c4a1";
+    base03 = "#bdae93";
+    base04 = "#665c54";
+    base05 = "#504945";
+    base06 = "#3c3836";
+    base07 = "#282828";
+    base08 = "#9d0006";
+    base09 = "#af3a03";
+    base0A = "#b57614";
+    base0B = "#79740e";
+    base0C = "#427b58";
+    base0D = "#076678";
+    base0E = "#8f3f71";
+    base0F = "#d65d0e";
+  };
+
+  defaultBase16 = {
+    scheme_name = null;
+    scheme_dir = null;
+    palette = defaultBase16Colors;
   };
 
   defaultNamedColors = {
-    window_border = defaultBase16Colors.base06;
     panel_fg = defaultBase16Colors.base04;
     panel_bg = defaultBase16Colors.base01;
+
     group_current_fg = defaultBase16Colors.base05;
     group_current_bg = defaultBase16Colors.base03;
     group_active_fg = defaultBase16Colors.base07;
     group_active_bg = defaultBase16Colors.base04;
     group_inactive_fg = defaultBase16Colors.base07;
     group_inactive_bg = defaultBase16Colors.base04;
-    powerline_fg = defaultBase16Colors.base01;
-    powerline_bg = [
-      defaultBase16Colors.base08
-      defaultBase16Colors.base09
-      defaultBase16Colors.base0A
-      defaultBase16Colors.base0B
-      defaultBase16Colors.base0C
-      defaultBase16Colors.base0D
-      defaultBase16Colors.base0E
-      defaultBase16Colors.base0F
-    ];
-  };
 
-  defaultExtensionProps = {
-    font = defaultTextFont.family;
-    fontsize = defaultTextFont.size;
-    foreground = defaultBase16Colors.base04;
-    background = defaultBase16Colors.base00;
-  };
+    widget_bg = [ defaultBase16Colors.base0F ];
+    widget_fg_dark = defaultBase16Colors.base00;
+    widget_fg_light = defaultBase16Colors.base04;
 
-  defaultLayoutProps = {
-    margin = 3;
-    border_width = 3;
-    border_focus = defaultBase16Colors.base02;
-    border_normal = defaultBase16Colors.base07;
-  };
+    window_border = defaultBase16Colors.base06;
 
-  defaultWidgetProps = {
-    font = defaultTextFont.family;
-    fontsize = defaultTextFont.size;
-    margin = 0;
-    padding = 0;
-    foreground = defaultBase16Colors.base00;
-    background = defaultBase16Colors.base07;
   };
 in
 {
   config = {
     xdg.configFile."desktop/default_theme.yaml".text = lib.generators.toYAML { } {
-      path = null;
-      logo = ""; # Nixos logo
+      bar = {
+        top = {
+          height = 22;
+          margin = [
+            defaultMargin
+            defaultMargin
+            0
+            defaultMargin
+          ];
+          opacity = 1.0;
+        };
+
+        bottom = {
+          height = 22;
+          margin = [
+            0
+            defaultMargin
+            defaultMargin
+            defaultMargin
+          ];
+          opacity = 1.0;
+        };
+      };
+
+      color = {
+        base16 = defaultBase16;
+        named = defaultNamedColors;
+      };
+
+      extension = {
+        font = defaultTextFont.family;
+        fontsize = defaultTextFont.size;
+        foreground = defaultBase16Colors.base04;
+        background = defaultBase16Colors.base00;
+      };
 
       font = {
         text = defaultTextFont;
@@ -106,37 +117,24 @@ in
         weather = defaultWeatherFont;
         logo = defaultLogoFont;
       };
-      bars = {
-        top = {
-          height = defaultBarHeight;
-          margin = [
-            8
-            8
-            4
-            8
-          ];
-          opacity = defaultBarOpacity;
-        };
-        bottom = {
-          height = defaultBarHeight;
-          margin = [
-            4
-            8
-            8
-            8
-          ];
-          opacity = defaultBarOpacity;
-        };
+
+      layout = {
+        margin = defaultMargin;
+        border_width = 3;
+        border_focus = defaultBase16Colors.base02;
+        border_normal = defaultBase16Colors.base07;
       };
 
-      foreground_dark = defaultBase16Colors.base00;
-      foreground_light = defaultBase16Colors.base04;
+      logo = ""; # Nixos logo
 
-      base16_scheme_colors = defaultBase16Colors;
-      named_colors = defaultNamedColors;
-      extension = defaultExtensionProps;
-      layout = defaultLayoutProps;
-      widget = defaultWidgetProps;
+      widget = {
+        font = defaultTextFont.family;
+        fontsize = defaultTextFont.size;
+        margin = 0;
+        padding = 0;
+        foreground = defaultBase16Colors.base00;
+        background = defaultBase16Colors.base07;
+      };
     };
   };
 }
