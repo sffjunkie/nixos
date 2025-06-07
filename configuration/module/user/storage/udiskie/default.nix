b7b1141b15c3cf55d742720ad7564097ff51d530
Simon Kennedy <sffjunkie+code@gmail.com>
2025-06-07 07:19:20 +0100

    Add assertion to check udisks2 is enabled

--- configuration/module/user/storage/udiskie/default.nix
+++ configuration/module/user/storage/udiskie/default.nix
@@ -15,6 +15,13 @@ in
   };
 
   config = mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = osConfig.looniversity.storage.udisks2.enable;
+        message = "Required machine service 'udisks2' is not enabled";
+      }
+    ];
+
     services.udiskie = {
       enable = true;
       tray = "never";
